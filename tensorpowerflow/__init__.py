ef067610da0e8564f0ab86ca3a917d9029ad59c6
Mauricio Salazar <emsd@kth.se>
2022-11-11 12:36:31 +0100

    Update version.

--- tensorpowerflow/__init__.py
+++ tensorpowerflow/__init__.py
@@ -2,7 +2,7 @@ from .utils import create_pandapower_net, generate_network, net_test
 from .grid import GridTensor
 from .gpu_tensor.gpu_interface import GPUPowerFlow
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __license__ = "MIT"
 __credits__ = ["Mauricio Salazar", "Juan Sebastian Giraldo"]
 __maintainer__ = "Mauricio Salazar"
